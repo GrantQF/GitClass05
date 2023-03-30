210c6c9e6170ef8d6ce81707a222720d55a48723
Grant <grantqfaulkner@gmail.com>
2023-03-30 15:29:38 -0400

    second commit

--- src/Gitclass05.java
+++ src/Gitclass05.java
@@ -1,6 +1,6 @@
 public class Gitclass05 {
     public static void main(String[] args) {
-        for (int i=0;i<10;i++){
+        for (int i=0;i<12;i++){
             System.out.println(i);
         }
     }
